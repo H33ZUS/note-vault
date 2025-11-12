910bc10ee892e132f24f35f020f367aba5387a77
Willyrys <w.stephan2005@icloud.com>
2025-11-12 11:39:54 +0100

    #4 Add user model

---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        maxLength: 30,
+        minLength: 1,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model("UserModel", userSchema);
+
+module.exports = User;
