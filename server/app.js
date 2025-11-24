66cde4d7e1ea694b1f89e25fb67f508f6d9575e3
Willyrys <w.stephan2005@icloud.com>
2025-11-24 17:30:55 +0100

    #22 Fix api versioning

--- server/app.js
+++ server/app.js
@@ -114,7 +114,7 @@ app.use(function(err, req, res, next) {
 app.listen(port, function(err) {
     if (err) throw err;
     console.log(`Express server listening on port ${port}, in ${env} mode`);
-    console.log(`Backend: http://localhost:${port}/api/v1/`);
+    console.log(`Backend: http://localhost:${port}/api/`);
     console.log(`Frontend (production): http://localhost:${port}/`);
 });
 
