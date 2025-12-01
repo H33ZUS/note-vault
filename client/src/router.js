6265cae071c4caffe53d7146324e42fced4e347c
Tobias Olloz <olloz@student.chalmers.se>
2025-12-01 11:10:49 +0100

    #37 add login page

--- client/src/router.js
+++ client/src/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
 import CreateSubjects from './views/Subjects.vue'
+import Login from './views/Login.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
-  { path: '/subjects/create', name: 'create-subjects', component: CreateSubjects }
+  { path: '/subjects/create', name: 'create-subjects', component: CreateSubjects },
+  { path: '/login', name: 'login', component: Login }
 ]
 
 const router = createRouter({
