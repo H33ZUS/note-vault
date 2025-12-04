51688cf779aa3b8e3b71b87b32b854188ed0371e
Willyrys <w.stephan2005@icloud.com>
2025-12-04 19:11:05 +0100

    #42 Add noteCommit validation

--- server/controllers/noteCommits.js
+++ server/controllers/noteCommits.js
@@ -2,10 +2,14 @@ const express = require("express");
 const NoteCommit = require("../models/noteCommit.js");
 const NoteFile = require("../models/noteFile.js")
 const { isAuthenticated, isAuthorized } = require("../middleware/auth.js");
+const { validateRequest } = require("../middleware/requestValidator");
+const { validateResponse } = require("../middleware/responseValidator.js");
+const val = require("../validations/noteCommitValidation");
+
 
 const router = express.Router({mergeParams: true}); 
 
-router.post("/", isAuthenticated, async(req, res) => {
+router.post("/", isAuthenticated, validateRequest(val.noteCommitRequestSchema), async(req, res, next) => {
     const {noteFileId} = req.params;
 
     try {
@@ -19,13 +23,26 @@ router.post("/", isAuthenticated, async(req, res) => {
             createdBy: req.user._id
         });
         await noteCommit.save();
-        res.status(201).json(noteCommit);
+
+        const noteCommitObj = noteCommit.toObject();
+        delete noteCommitObj.__v;
+
+        if (noteCommitObj._id && noteCommitObj.noteFileId) {
+            noteCommitObj._id = noteCommitObj._id.toString();
+            noteCommitObj.noteFileId = noteCommitObj.noteFileId.toString();
+            noteCommitObj.createdBy = noteCommitObj.createdBy.toString();
+        }
+
+        res.locals.data = noteCommitObj;
+        next();
     } catch (err) {
         res.status(400).json({error: err.message});
     }
+}, validateResponse(val.noteCommitResponseSchema), (req, res) => {
+    res.status(201).json(res.locals.data);
 });
 
-router.get("/", async(req, res) => {
+router.get("/", async(req, res, next) => {
 
     const {noteFileId} = req.params;
 
@@ -42,14 +59,30 @@ router.get("/", async(req, res) => {
     }
 
     try {
-        const noteCommit = await NoteCommit.find({noteFileId: noteFileId}).sort(sortOptions);
-        if (noteCommit.length == 0) {
+        const noteCommits = await NoteCommit.find({noteFileId: noteFileId}).sort(sortOptions).select("-__v");
+        if (noteCommits.length == 0) {
             return res.status(404).json({error: "No NoteCommits found for this NoteFile"});
         }
-        res.json(noteCommit);
+
+        const finalData = noteCommits.map(noteCommit => {
+           const noteCommitObj = noteCommit.toObject();
+
+            if (noteCommitObj._id && noteCommitObj.noteFileId) {
+                noteCommitObj._id = noteCommitObj._id.toString();
+                noteCommitObj.noteFileId = noteCommitObj.noteFileId.toString();
+                noteCommitObj.createdBy = noteCommitObj.createdBy.toString();
+            }
+
+            return noteCommitObj;
+        });
+
+        res.locals.data = finalData;
+        next();
     } catch (err) {
         res.status(404).json({error: err.message});
     }
+}, validateResponse(val.noteCommitResponseSchema), (req, res) => {
+    res.status(200).json(res.locals.data);
 });
 
 // DELETE ONE NOTE FILE
@@ -70,7 +103,7 @@ router.delete("/:id", isAuthenticated, async(req, res) => {
 });
 
 // UPDATE ONE VARIABLE OF A NOTE FILE
-router.patch("/:id", isAuthenticated, async(req, res) => {
+router.patch("/:id", isAuthenticated, validateRequest(val.noteCommitPatchRequestSchema), async(req, res, next) => {
     userId = req.user._id;
     
     try {
@@ -84,12 +117,23 @@ router.patch("/:id", isAuthenticated, async(req, res) => {
             return res.status(403).json({message: "You are not authorized to update another user's note commit"})
         }
 
-        const noteCommit = await NoteCommit.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        const noteCommit = await NoteCommit.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true}).select("-__v");
 
-        res.json(noteCommit);
+        const noteCommitObj = noteCommit.toObject();
+
+        if (noteCommitObj._id && noteCommitObj.noteFileId) {
+            noteCommitObj._id = noteCommitObj._id.toString();
+            noteCommitObj.noteFileId = noteCommitObj.noteFileId.toString();
+            noteCommitObj.createdBy = noteCommitObj.createdBy.toString();
+        }
+
+        res.locals.data = noteCommitObj;
+        next();
     } catch (err) {
         res.status(400).send(err.message);
     }
+}, validateResponse(val.noteCommitResponseSchema), (req, res) => {
+    res.status(200).json(res.locals.data);
 });
 
 // use same path for the rest of the operations but with /:commitId 
---
+++ server/validations/noteCommitValidation.js
@@ -0,0 +1,29 @@
+const Joi = require("joi");
+
+// REQUEST SCHEMAS
+const noteCommitRequestSchema = Joi.object({
+    note: Joi.string().min(1).required(),
+    topic: Joi.string().min(5).max(100).required()
+}).unknown(false);
+
+const noteCommitPatchRequestSchema = Joi.object({
+    note: Joi.string().min(1).required()
+}).unknown(false);
+
+// RESPONSE SCHEMAS
+const noteCommitResponseSchema = Joi.object({
+    _id: Joi.string().hex().length(24).required(),
+    dislikes: Joi.number().required(),
+    likes: Joi.number().required(),
+    createdBy: Joi.string().hex().length(24).required(),
+    note: Joi.string().min(1).required(),
+    topic: Joi.string().min(5).max(100).required(),
+    noteFileId: Joi.string().hex().length(24).required(),
+    createdAt: Joi.date().required()
+}).unknown(false);
+
+module.exports = {
+    noteCommitRequestSchema,
+    noteCommitPatchRequestSchema,
+    noteCommitResponseSchema
+}
